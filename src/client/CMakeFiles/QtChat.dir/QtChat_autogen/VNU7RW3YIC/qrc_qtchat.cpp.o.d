c8f2e4df8975e442065ae92480b316114920e912
dadawer-web <3902764053@qq.com>
2026-01-19 01:03:34 +0800

    Fix: 彻底清理所有层级的 CMake 中间文件

--- src/client/CMakeFiles/QtChat.dir/QtChat_autogen/VNU7RW3YIC/qrc_qtchat.cpp.o.d
+++
@@ -1,3 +0,0 @@
-src/client/CMakeFiles/QtChat.dir/QtChat_autogen/VNU7RW3YIC/qrc_qtchat.cpp.o: \
- /home/xmy/code/src/client/QtChat_autogen/VNU7RW3YIC/qrc_qtchat.cpp \
- /usr/include/stdc-predef.h
--- src/client/CMakeFiles/QtChat.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for QtChat.
-# This may be replaced when dependencies are built.
